6a45f7dc54506285918d3ec60b393068b1a8cc6e
abidex4yemi <abidex4yemi@gmail.com>
2020-11-03 14:45:37 +0100

    Update ip

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,2 @@
-export const BASE_USER_API_URL = 'http://167.172.136.33:3002/api/v1';
-export const BASE_TASK_API_URL = 'http://167.172.136.33:3001/api/v1';
+export const BASE_USER_API_URL = 'http://68.183.97.149:3002/api/v1';
+export const BASE_TASK_API_URL = 'http://68.183.97.149:3001/api/v1';
